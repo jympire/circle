668755f45b612a95ca7d086dfac3e561f06970a9
Jim <wefactoryou@gmail.com>
2018-02-14 08:47:00 -0800

    update mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'https://lit-mesa-56715.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://circle.fit' }
   
   ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
