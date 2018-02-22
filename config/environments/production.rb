04ccbb21acb67e2399e3399c6355a0bea813192a
Jim <wefactoryou@gmail.com>
2018-02-21 16:56:30 -0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'http://circle.fit' }
+  config.action_mailer.default_url_options = { protocol: 'https', :host => 'https://circle.fit' }
   
   ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
