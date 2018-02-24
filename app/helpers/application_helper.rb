7d53abc0a6276654693d80cfb532ce76c71636ad
Jim <wefactoryou@gmail.com>
2018-02-24 13:54:32 -0800

    update https

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def avatar_url(user)
     if user.image
-      "http://graph.facebook.com/#{user.uid}/picture?type=large"
+      "https://graph.facebook.com/#{user.uid}/picture?type=large"
     else
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-      "http://gravatar.com/avatar/#{gravatar_id}.png"
+      "https://gravatar.com/avatar/#{gravatar_id}.png"
     end
   end
   
